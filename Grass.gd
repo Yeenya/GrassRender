e9a6298374d5bcaaaa1ffd790267a907eed65c7a
Jan Borecky <honza.borecky@seznam.cz>
2024-04-24 15:03:00 +0200

    Interaction with 1 objects

---
+++ Grass.gd
@@ -0,0 +1,16 @@
+@tool
+extends MultiMeshInstance3D
+
+@export var character_path := NodePath()
+@onready var _character: Node3D = get_node(character_path)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	material_override.set_shader_parameter(
+		"character_position", _character.global_transform.origin
+	)
